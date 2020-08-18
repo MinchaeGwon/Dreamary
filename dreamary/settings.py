1ed59518b502554745ac3310d210b7e7aa8f96eb
jun <solar3070@naver.com>
2020-08-18 20:56:25 +0900

    second commit

--- dreamary/settings.py
+++ dreamary/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -107,3 +108,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dreamaryapp', 'static')
+] 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- dreamary/urls.py
+++ dreamary/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 import dreamaryapp.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', dreamaryapp.views.home, name='home')
-]
+    path('', include('dreamaryapp.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
